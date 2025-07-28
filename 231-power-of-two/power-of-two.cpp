329b4426dddd4b0ed37065c1e5252b9a7abacda6
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2025-07-28 18:39:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.32%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -3,13 +3,9 @@ public:
     
     bool isPowerOfTwo(int n) {
         if(n==1) return 1;
-        if(n==0) return 0;
-        int m=n;
-        while(m!=1){
-            if(m%2!=0) return false;
-            m/=2;
-        }
-        return true;
+        if(n==0 || n%2!=0) return 0;
+       
+        return  isPowerOfTwo(n/2);
         
     }
 };
