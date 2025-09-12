7686e5cc1a4abdddbc2f91ecc1170e18760a69ac
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2025-09-12 14:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (64.69%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
+      
 
+        //if(i>=nums.size()) return 0;
+        //if(dp[i]!=-1) return dp[i];
+       // int f=nums[i]+helper(nums,i+2,dp);
+       // int s=helper(nums,i+1,dp);
+       // return dp[i]=max(s,f);
+    //}
 
-    int helper(vector<int>& nums,int i,vector<int>&dp){
-        if(i>=nums.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int f=nums[i]+helper(nums,i+2,dp);
-        int s=helper(nums,i+1,dp);
-        return dp[i]=max(s,f);
+    int bottomup(vector<int>& nums,vector<int>&dp,int n){
+        if(n==1) return nums[0];
+        if(n==0) return 0;
+        
+        
+        dp[n-1]=nums[n-1];
+        dp[n-2]=max(nums[n-1],nums[n-2]);
+        for(int i=n-3; i>=0; i--){
+            dp[i]=max(dp[i+1],dp[i+2]+nums[i]);
+
+        }
+        return dp[0];
     }
+
+
+
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return helper(nums,0,dp);
+        int n=nums.size();
+          vector<int>dp(nums.size());
+      
+        return bottomup(nums,dp,n);
         
     }
 };
