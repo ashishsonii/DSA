6a29b2a721cf2f406cbc3381510e8d93db3b13ec
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2025-09-15 13:53:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (99.97%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int dp[31][1001];
+    
+    int helper(int n, int k, int target){
+
+        if (n == 0 && target == 0) return 1;
+        if (n == 0 || target <= 0) return 0;
+        if (dp[n][target] != -1) return dp[n][target];
+
+
+        
+        long long x=0;
+
+        for(int i=1; i<=k; i++){
+            x+= helper(n-1, k,target-i);
+            x%=1000000007;
+
+
+      }
+        
+        return dp[n][target]=x;
+        
+    }
+
+    int numRollsToTarget(int n, int k, int target) {
+        
+
+        memset(dp, -1, sizeof(dp)); 
+        
+       
+        return helper(n, k, target);
+
+        
+    }
+};
