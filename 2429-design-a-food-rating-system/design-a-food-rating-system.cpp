1fd2c5a7985310012f24b7b92fee4ae5a2366027
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2025-09-17 15:28:34 +0530

    Time: 160 ms (38.23%) | Memory: 174.2 MB (21.05%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -2,18 +2,11 @@ class FoodRatings {
 public:
 
 
-struct cmp {
-    bool operator()(const pair<int,string>& a, const pair<int,string>& b) const {
-        if (a.first == b.first) {
-            return a.second < b.second; // smaller name first if same rating
-        }
-        return a.first > b.first; // higher rating first
-    }
-};
+
 
 
         unordered_map<string,int > fodrat;
-        unordered_map<string,set<pair<int,string>,cmp>> cuisratfood;
+        unordered_map<string,set<pair<int,string>>> cuisratfood;
         unordered_map<string,string>foodcuis;
         
 
@@ -28,7 +21,7 @@ struct cmp {
         for(int i=0; i<foods.size(); i++){
             fodrat[foods[i]]=ratings[i];
             foodcuis[foods[i]]=cuisines[i];
-            cuisratfood[cuisines[i]].insert({ratings[i], foods[i]});
+            cuisratfood[cuisines[i]].insert({-ratings[i], foods[i]});
 
 
         }
@@ -39,8 +32,8 @@ struct cmp {
         int oldrat=fodrat[food];
         fodrat[food]=newRating;
         string curcuis=foodcuis[food];
-        cuisratfood[curcuis].erase({oldrat,food});
-        cuisratfood[curcuis].insert({newRating,food});
+        cuisratfood[curcuis].erase({-oldrat,food});
+        cuisratfood[curcuis].insert({-newRating,food});
         
 
         
