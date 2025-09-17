99da7560235687ae829eb330e91a6dba933d66af
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2025-09-17 18:38:33 +0530

    Time: 8 ms (88.87%) | Memory: 8.1 MB (94.30%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
+int arr[1005][31];
 
-    int dp[31][1001];
+long long helper(int n, int k, int target){
+   
+    if(n==0 ) return  target==0;
+    if(arr[target][n]!=-1) return arr[target][n];
     
-    int helper(int n, int k, int target){
-
-        if (n == 0 && target == 0) return 1;
-        if (n == 0 || target <= 0) return 0;
-        if (dp[n][target] != -1) return dp[n][target];
-
-
-        
-        long long x=0;
-
-        for(int i=1; i<=k; i++){
-            x+= helper(n-1, k,target-i);
-            x%=1000000007;
-
-
-      }
-        
-        return dp[n][target]=x;
-        
+     long long ways=0;
+    for(int i=1; i<=k; i++){
+    if(target-i>=0 ){
+        ways+=helper(n-1,k,target-i);
     }
+ }
+    return arr[target][n]=ways%(1000000000+7);
+}
 
     int numRollsToTarget(int n, int k, int target) {
+        memset(arr,-1,sizeof(arr));
         
-
-        memset(dp, -1, sizeof(dp)); 
-        
-       
-        return helper(n, k, target);
-
+        return helper(n,k,target);
         
     }
 };
