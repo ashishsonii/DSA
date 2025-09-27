941abb20142d8c17925b92df155383605417c46b
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2025-09-27 16:24:26 +0530

    Time: 295 ms (18.34%) | Memory: 172.8 MB (5.19%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -24,7 +24,21 @@ public:
     }
 
     long long maxAlternatingSum(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return helper(nums, 0 ,true);
+        // return helper(nums, 0 ,true);
+
+
+        /// bottom up approach;
+
+        int n= nums.size();
+        vector<vector<long long>>dp(n+1,vector<long long>(2,0));
+
+        for(int i=1; i<n+1; i++){
+            dp[i][0]=max(+dp[i-1][1]-nums[i-1],dp[i-1][0]);
+            dp[i][1]=max(nums[i-1]+dp[i-1][0],dp[i-1][1]);
+
+        }
+        return max(dp[n][0],dp[n][1]);
+
+       
     }
 };
