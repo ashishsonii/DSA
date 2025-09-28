4d2c0d6142d228da4c026c4c7200f93411446671
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2025-09-28 13:15:00 +0530

    Time: 4 ms (65.53%) | Memory: 25.7 MB (14.34%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    bool check(int a,int b,int c){
-        if(a==0 ||b==0 || c==0) return false;
-        if(a+b<=c) return false;
-        if(a+c<=b) return false;
-        if(b+c<=a) return false;
+    bool check(int a, int b, int c) {
+        if (a == 0 || b == 0 || c == 0)
+            return false;
+        if (a + b <= c)
+            return false;
+        if (a + c <= b)
+            return false;
+        if (b + c <= a)
+            return false;
         return true;
     }
     int largestPerimeter(vector<int>& nums) {
-        
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
 
-        for(int i=n-1; i>=0; i--){
-            int a=nums[i];
-            for(int j=i-1; j>=1; j--){
-                int b=nums[j];
-                int c=nums[j-1];
-                if(check(a,b,c)) return a+b+c;
-                
-            }
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
 
+        for (int i = n - 1; i >= 2; i--) {
+
+            int a = nums[i];
+            int j = i - 1;
+
+            int b = nums[j];
+            int c = nums[j - 1];
+            if (check(a, b, c))
+                return a + b + c;
         }
-        return 0;
-    }
-
-        
     
-};
+    return 0;
+}
+}
+;
