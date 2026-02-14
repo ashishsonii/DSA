f0838cca5e86f71c99c09b3e7ae076cc2d91b204
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2026-02-14 18:09:03 +0530

    Time: 2 ms (15.45%) | Memory: 9.6 MB (63.40%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int m) {
-        
+    vector<vector<int>> generate(int r) {
 
-        vector<vector<int>>v;
-        for(int i=0; i<m; i++){
-            vector<int> temp(i+1);
-           v.push_back(temp);
-        }
-     
-       
+        vector<vector<int>> v;
         
-        
-        
-        
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<=i; j++){
-                if(j==0|| j==i){
-                    v[i][j]=1;
+        v.push_back({1});
 
-                }
-                else v[i][j]=v[i-1][j]+v[i-1][j-1];
+        for(int i=1; i<r; i++){
+            vector<int>t(i+1,1);
+            for(int j=1; j<i; j++){
+                t[j]=v[i-1][j];
+                if(j-1>=0) t[j]+=v[i-1][j-1];
                 
-
             }
+            v.push_back(t);         
+
         }
         return v;
         
