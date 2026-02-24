a5655c6089fb906d32f02c42a216cd47a330b56c
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2026-02-24 18:24:05 +0530

    Time: 3 ms (33.36%) | Memory: 10.7 MB (39.78%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    set<vector<int>> st;
-    vector<vector<int>> ans;
-    vector<int> v;
 
+   vector<vector<int>> ans;
 
-    void sub(vector<int>& nums ,vector<int> &v,int start){
-
-       
-        for(int i=start; i<nums.size(); i++){
-        
-        v.push_back(nums[i]);
-            
-        if(st.find(v)==st.end()){
-            ans.push_back(v);
-            st.insert(v);
-        }
-        sub(nums,v,i+1);
-        v.pop_back();
-
-        }
+   void helper(vector<int>& nums,int start,vector<int> &temp ){   
+    
+    ans.push_back(temp);  
+    
+    for(int i=start; i<nums.size(); i++){
+        if(i>start && nums[i-1]==nums[i]) continue;
+        temp.push_back(nums[i]);
+        helper(nums,i+1,temp);
+        temp.pop_back();
 
     }
+    return;
+   
+    
+
+
+} 
+
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> v;
-        sort(nums.begin(),nums.end());
-        ans.push_back(v);
-        sub(nums,v,0);
+        
+        vector<int> temp;
+        sort(nums.begin(), nums.end()); 
+
+        helper(nums,0,temp);
         return ans;
+
 
 
         
