3f5dfeb74710c46bbb00bfa1ecae27976084bbf1
ashish soni <133988917+ashishsonii@users.noreply.github.com>
2026-03-07 16:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (28.80%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> move{{0, 1}, {1, 0}};
-
-   
-    int m;
-    int n;
-
-   int  path(int i, int j,vector<vector<int>> &dp) {
-        if (i >= m || j >= n)
-            return 0;
+    int c=0;
+  
+    int help(int i, int j,int m, int n, vector<vector<int>>&dp){
+        if(i==m-1 && j==n-1) return 1;
+        
+        if(i>=m || j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if (i == m - 1 && j == n - 1) {
-            return 1;
-        }
 
-
-        int left=path(i + move[0][0], j + move[0][1],dp);
-        int right =path(i + move[1][0], j + move[1][1],dp);
-        return dp[i][j]=left+right;
+        int down=help(i+1,j,m,n,dp);
+        int right=help(i,j+1,m,n,dp);
+        
+        return  dp[i][j]=down+right;
     }
+    int uniquePaths(int m, int n) {
+          vector<vector<int>> dp(m,vector<int>(n,-1));
 
-    int uniquePaths(int l, int k) {
-      
-        m = l;
-        n = k;
-        vector<vector<int>>dp(m,vector<int> (n,-1));
-       
-        return path(0, 0,dp);
+        
+        return help(0,0,m,n,dp);
         
     }
 };
